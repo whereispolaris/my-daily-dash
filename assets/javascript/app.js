919897cdc6f6af27423d26f118110e5d966bbaae
k2k <kadonna@wonderkul.com>
2019-07-13 13:34:14 -0500

    Basic congratulations message with api results.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,32 @@
 $(document).ready(function () {
-    console.log("jquery works");
-    $(".modal").modal();
+
+//Start code for app
+console.log("jquery works");
+
+//add Task modal
+$(".modal").modal();
+       
+//This displays the Congratulations message
+   
+  //  var boredIdea="";
+    var queryURL = "https://www.boredapi.com/api/activity/"
+
+    $(function() {
+        $( "#dialog" ).dialog();
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+          }).then(function(response) {
+
+            var boredIdea = response.activity;
+
+            apiMsg.append(boredIdea);
+        });
+
+  
+  
+//This is the end of the doc ready function
+});
+
 });
